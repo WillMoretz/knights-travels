e223905cf52a03aeeaaa65a1d185dd77009435d6
Will Moretz <whmoretz@gmail.com>
2023-03-21 21:04:48 -0400

    created knight object

--- travel.js
+++ travel.js
@@ -16,11 +16,13 @@ const chessBoard = (() => {
     return board;
   }
 
-  function print() {
+  function print(knightLocation) {
     board.forEach((row) => {
       let strRow = "";
       row.forEach((square) => {
-        strRow += `[${square[0]} ${square[1]}]`;
+        if (square[0] === knightLocation[0] && square[1] === knightLocation[1])
+          strRow += "[ N ]";
+        else strRow += `[${square[0]} ${square[1]}]`;
       });
       console.log(strRow);
     });
@@ -29,4 +31,47 @@ const chessBoard = (() => {
   return { getBoard, print };
 })();
 
-chessBoard.print();
+const knight = (() => {
+  let location = [7, 6];
+  function setLocation(newLocation) {
+    location = newLocation;
+  }
+  function getLocation() {
+    return location;
+  }
+
+  function listPossibleMoves(currentLocation) {
+    const possibleMoves = [];
+
+    // Vertical Up
+    if (currentLocation[0] - 2 >= 0 && currentLocation[1] - 1 >= 0)
+      possibleMoves.push([currentLocation[0] - 2, currentLocation[1] - 1]);
+    if (currentLocation[0] - 2 >= 0 && currentLocation[1] + 1 <= 7)
+      possibleMoves.push([currentLocation[0] - 2, currentLocation[1] + 1]);
+
+    // Vertical Down
+    if (currentLocation[0] + 2 <= 7 && currentLocation[1] - 1 >= 0)
+      possibleMoves.push([currentLocation[0] + 2, currentLocation[1] - 1]);
+    if (currentLocation[0] + 2 <= 7 && currentLocation[1] + 1 <= 7)
+      possibleMoves.push([currentLocation[0] + 2, currentLocation[1] + 1]);
+
+    // Horizontal Right
+    if (currentLocation[0] - 1 >= 0 && currentLocation[1] + 2 <= 7)
+      possibleMoves.push([currentLocation[0] - 1, currentLocation[1] + 2]);
+    if (currentLocation[0] + 1 <= 7 && currentLocation[1] + 2 <= 7)
+      possibleMoves.push([currentLocation[0] + 1, currentLocation[1] + 2]);
+
+    // Horizontal Left
+    if (currentLocation[0] - 1 >= 0 && currentLocation[1] - 2 >= 0)
+      possibleMoves.push([currentLocation[0] - 1, currentLocation[1] - 2]);
+    if (currentLocation[0] + 1 <= 7 && currentLocation[1] - 2 >= 0)
+      possibleMoves.push([currentLocation[0] + 1, currentLocation[1] - 2]);
+
+    return possibleMoves;
+  }
+
+  return { setLocation, getLocation, listPossibleMoves };
+})();
+
+chessBoard.print(knight.getLocation());
+console.log(knight.listPossibleMoves(knight.getLocation()));
