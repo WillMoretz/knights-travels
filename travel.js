2021d53fb118dbb41ef62103ee6e20df5a905a9a
Will Moretz <whmoretz@gmail.com>
2023-03-21 21:53:39 -0400

    added pathFinder object

--- travel.js
+++ travel.js
@@ -16,23 +16,27 @@ const chessBoard = (() => {
     return board;
   }
 
+  function squaresMatch(square1, square2) {
+    if (square1[0] === square2[0] && square1[1] === square2[1]) return true;
+    return false;
+  }
+
   function print(knightLocation) {
     board.forEach((row) => {
       let strRow = "";
       row.forEach((square) => {
-        if (square[0] === knightLocation[0] && square[1] === knightLocation[1])
-          strRow += "[ N ]";
+        if (squaresMatch(square, knightLocation)) strRow += "[ N ]";
         else strRow += `[${square[0]} ${square[1]}]`;
       });
       console.log(strRow);
     });
   }
 
-  return { getBoard, print };
+  return { getBoard, print, squaresMatch };
 })();
 
 const knight = (() => {
-  let location = [7, 6];
+  let location = [0, 0];
   function setLocation(newLocation) {
     location = newLocation;
   }
@@ -73,5 +77,48 @@ const knight = (() => {
   return { setLocation, getLocation, listPossibleMoves };
 })();
 
+const pathFinder = (() => {
+  const node = (location, parent) => ({
+    location,
+    parent,
+    addChild(child) {
+      this.children.push(child);
+    },
+  });
+
+  function findPath(start, target) {
+    let path = "";
+    const baseNode = node(start, null);
+    const queue = [baseNode];
+
+    while (true) {
+      const currentNode = queue.shift();
+      if (chessBoard.squaresMatch(currentNode.location, target)) {
+        // eslint-disable-next-line no-inner-declarations
+        function recur(retracedNode) {
+          if (retracedNode.parent === null)
+            return `Path: [${retracedNode.location[0]}, ${retracedNode.location[1]}]`;
+
+          return `${recur(retracedNode.parent)} , [${
+            retracedNode.location[0]
+          }, ${retracedNode.location[1]}]`;
+        }
+        path += recur(currentNode);
+        break;
+      }
+
+      for (const move of knight.listPossibleMoves(currentNode.location)) {
+        // console.log(move);
+        queue.push(node(move, currentNode));
+      }
+    }
+
+    return path;
+  }
+
+  return { findPath };
+})();
+
+knight.setLocation([4, 7]);
 chessBoard.print(knight.getLocation());
-console.log(knight.listPossibleMoves(knight.getLocation()));
+console.log(pathFinder.findPath(knight.getLocation(), [7, 7]));
